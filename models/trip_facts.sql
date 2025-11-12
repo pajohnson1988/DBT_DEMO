073dd076356fc7b6d1cc41af219bb6b5db11775a
pajohnson1988@gmail.com <pajohnson1988@gmail.com>
2025-11-12 19:50:52 +0000

    trip fact model created

---
+++ models/trip_facts.sql
@@ -0,0 +1,20 @@
+with TRIPS as (
+
+    select  
+    ride_id
+    ,rideable_type
+    ,date(TO_TIMESTAMP(started_at)) as trip_date
+    ,start_station_id
+    ,end_station_id
+    ,member_csual as member_casual
+    ,TIMESTAMPDIFF(SECOND,TO_TIMESTAMP(started_at), TO_TIMESTAMP(ended_at)) as trip_duration_seconds
+    from {{ source('demo', 'bike') }}
+ 
+    where ride_id <> 'ride_id'
+
+    limit 10
+)
+
+
+ select *
+ from TRIPS
